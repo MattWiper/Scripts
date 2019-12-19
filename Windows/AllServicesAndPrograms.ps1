e3525447fd46aa010ebc5e6ec6a372f2118b1fbd
Matt Wiper <mattwiper@gmail.com>
2019-12-19 09:30:15 -0400

    Create AllServicesAndPrograms.ps1

---
+++ Windows/AllServicesAndPrograms.ps1
@@ -0,0 +1,3 @@
+Get-Service | Out-File -FilePath .\Services.txt
+
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File .\Programs.txt
